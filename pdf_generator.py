59d9da88ab7a33f6d0892829402c2fa82d7f0d0d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-02-20 21:45:18 -0300

    FEAT(pdf_generator): Criando classe PDF onde podemos gerar arquivos em pdf

---
+++ pdf_generator.py
@@ -0,0 +1,16 @@
+from fpdf import FPDF
+
+class PDF(FPDF):
+    def header(self):
+        self.set_font('Arial', 'B', 12)
+        self.cell(0, 10, self.title, 0, 1, 'C')
+
+    def chapter_title(self, title):
+        self.set_font('Arial', 'B', 12)
+        self.cell(0, 10, title, 0, 1, 'L')
+        self.ln(10)
+
+    def chapter_body(self, body):
+        self.set_font('Arial', '', 12)
+        self.multi_cell(0, 10, body)
+        self.ln()
